a50344e804572be30d721cb80d82deb0ed5c0104
lymeYo <lyme1878@mail.ru>
2024-08-22 16:47:39 +0300

    fix

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,11 +2,12 @@ import localFont from "next/font/local"
 
 // Font files can be colocated inside of `app`
 export const lack = localFont({
-  src: "../assets/fonts/Lack-Regular.ttf",
+  src: "../assets/fonts/Lack-Regular.otf",
   variable: "--font-lack",
   style: "normal",
   display: "swap"
 })
+
 export const steppe = localFont({
   src: "../assets/fonts/Steppe.otf",
   variable: "--font-steppe",
