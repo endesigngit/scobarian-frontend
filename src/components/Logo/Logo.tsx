9a98b699a1791114672cbc524d75ecedee8ada1d
chv-roman <chv-roman@mail.ru>
2024-10-29 22:31:21 +0300

    -add: Добавлена анимация появления ссылок в шапке;

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,11 +3,13 @@ import Link from "next/link"
 import styles from "./Logo.module.css"
 import { clsx } from "clsx"
 import { CSSTransition } from "react-transition-group"
+import { useRef } from "react"
 
 export default function Logo() {
+  const nodeRef = useRef(null)
   return (
-    <CSSTransition in appear timeout={300} classNames={{ ...styles }}>
-      <div className={styles.logo}>
+    <CSSTransition nodeRef={nodeRef} in appear timeout={300} classNames={{ ...styles }}>
+      <div className={styles.logo} ref={nodeRef}>
         <Link href="/" className={clsx(styles.logo_title)}>
           I’am skobarian
         </Link>
--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -3,14 +3,18 @@ import Link from "next/link"
 import styles from "./MainHeader.module.css"
 import { clsx } from "clsx"
 import OffcanvasMenuTab from "../OffcanvasMenuTab/OffcanvasMenuTab"
-import { MouseEvent, useEffect, useState } from "react"
+import { MouseEvent, useEffect, useRef, useState } from "react"
 import HeaderCart from "../HeaderCart/HeaderCart"
 import OffcanvasMenuMob from "../OffcanvasMenuMob/OffcanvasMenuMob"
-import { useBoundStore } from "@/store/StoreProvider"
+import { CSSTransition } from "react-transition-group"
+
+import { usePathname } from "next/navigation"
 
 export default function MainHeader() {
   const [IsActive, setActive] = useState<boolean>(false)
   const [IsTab, setTab] = useState<boolean>(false)
+  const [path, setPath] = useState<string>("")
+  const pathname = usePathname()
 
   const menuClickHandler = (evt: MouseEvent<HTMLAnchorElement>) => {
     if (IsTab) {
@@ -22,7 +26,17 @@ export default function MainHeader() {
     if (window.document.documentElement.clientWidth >= 950 && window.document.documentElement.clientWidth <= 1024) {
       setTab(true)
     }
-  }, [])
+    setPath(pathname)
+  }, [pathname])
+
+  const nodeReflink1 = useRef(null)
+  const nodeReflink2 = useRef(null)
+  const nodeReflink3 = useRef(null)
+  const nodeReflink4 = useRef(null)
+  const nodeReflink5 = useRef(null)
+  const nodeReflink6 = useRef(null)
+
+
   return (
     <header
       className={clsx(styles.header, IsActive && styles.header__active)}
@@ -32,14 +46,18 @@ export default function MainHeader() {
       <nav className={styles.header__nav}>
         <ul className={clsx("main_grid", styles.main_menu)}>
           <li className={clsx(styles.main_menu__item, "main_col_1")}>
-            <Link href="/about" className={styles.main_menu__link}>
-              О нас
-            </Link>
+            <CSSTransition nodeRef={nodeReflink1} in={pathname != path} timeout={300} classNames={"anim-1"}>
+              <Link href="/about" className={styles.main_menu__link} ref={nodeReflink1}>
+                О нас
+              </Link>
+            </CSSTransition>
           </li>
           <li className={clsx(styles.main_menu__item, "main_col_2")}>
-            <Link href="/catalog" className={styles.main_menu__link} onClick={menuClickHandler}>
-              Каталог
-            </Link>
+            <CSSTransition nodeRef={nodeReflink2} in={pathname != path} timeout={500} classNames={"anim-2"}>
+              <Link href="/catalog" className={styles.main_menu__link} onClick={menuClickHandler} ref={nodeReflink2}>
+                Каталог
+              </Link>
+            </CSSTransition>
             <ul className={styles.main_submenu}>
               <li className={styles.main_submenu__item}>
                 <Link href="/catalog/womens" className={styles.main_submenu__title}>
@@ -76,20 +94,28 @@ export default function MainHeader() {
             </ul>
           </li>
           <li className={clsx(styles.main_menu__item, "main_col_3")}>
-            <Link href="/tailoring" className={styles.main_menu__link}>
-              Пошив
-            </Link>
+            <CSSTransition nodeRef={nodeReflink3} in={pathname != path} timeout={700} classNames={"anim-3"}>
+              <Link href="/tailoring" className={styles.main_menu__link} ref={nodeReflink3}>
+                Пошив
+              </Link>
+            </CSSTransition>
           </li>
           <li className={clsx(styles.main_menu__item, "main_col_4")}>
-            <Link href="/to-buyers" className={styles.main_menu__link}>
-              Покупателям
-            </Link>
+            <CSSTransition nodeRef={nodeReflink4} in={pathname != path} timeout={700} classNames={"anim-4"}>
+              <Link href="/to-buyers" className={styles.main_menu__link} ref={nodeReflink4}>
+                Покупателям
+              </Link>
+            </CSSTransition>
           </li>
           <li className={clsx(styles.main_menu__item, styles.whith_carts, "main_col_5")}>
-            <Link href="/contacts" className={styles.main_menu__link}>
-              Контакты
-            </Link>
-            <HeaderCart isActiveHeader={IsActive} />
+            <CSSTransition nodeRef={nodeReflink5} in={pathname != path} timeout={700} classNames={"anim-5"}>
+              <Link href="/contacts" className={styles.main_menu__link} ref={nodeReflink5}>
+                Контакты
+              </Link>
+            </CSSTransition>
+            <CSSTransition nodeRef={nodeReflink6} in={pathname != path} timeout={700} classNames={"anim-6"}>
+              <HeaderCart isActiveHeader={IsActive} />
+            </CSSTransition>
           </li>
         </ul>
       </nav>
