4f685ac73463b313b6c1ac1139ebc54e556fc035
chv-roman <chv-roman@mail.ru>
2024-11-08 16:23:22 +0300

    -fix: Исправлен Offcanvas компонент

--- src/components/Offcanvas/Offcanvas.tsx
+++ src/components/Offcanvas/Offcanvas.tsx
@@ -1,27 +1,29 @@
-import { clsx } from "clsx";
-import styles from "./Offcanvas.module.css";
-import { ReactNode, useState, useEffect } from "react";
+import { clsx } from "clsx"
+import styles from "./Offcanvas.module.css"
+import { ReactNode, useState, useEffect } from "react"
 
 type OffcanvasProps = {
-  isActive: boolean;
-  closeHandler: (value: boolean) => void;
-  title: string;
-  children: ReactNode;
-};
+  isActive: boolean
+  closeHandler: (value: boolean) => void
+  title: string
+  children: ReactNode
+}
 
 export default function Offcanvas({ isActive, closeHandler, title = "Offcanvas", children }: OffcanvasProps) {
-  const [isClosing, setIsClosing] = useState(false);
+  const [isClosing, setIsClosing] = useState(false)
 
   useEffect(() => {
     if (!isActive) {
-      setIsClosing(true);
-      const timer = setTimeout(() => setIsClosing(false), 600); 
-      return () => clearTimeout(timer);
+      setIsClosing(true)
+      const timer = setTimeout(() => setIsClosing(false), 600)
+      return () => clearTimeout(timer)
     }
-  }, [isActive]);
+  }, [isActive])
 
   return (
-    <div className={clsx(styles.offcanvas, isActive ? styles.offcanvas__active : isClosing && styles.offcanvas__inactive)}>
+    <div
+      className={clsx(styles.offcanvas, isActive ? styles.offcanvas__active : isClosing && styles.offcanvas__inactive)}
+    >
       <div className={styles.overlay} onClick={() => closeHandler(false)}></div>
       <div className={styles.offcanvas_body}>
         <div className={styles.offcanvas_nav}>
@@ -33,5 +35,5 @@ export default function Offcanvas({ isActive, closeHandler, title = "Offcanvas",
         <div className={styles.offcanvas_content}>{children}</div>
       </div>
     </div>
-  );
+  )
 }
