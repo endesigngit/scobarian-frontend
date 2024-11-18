84a33fd41f256fa5352a5167f7de46bcd55019f7
chv-roman <chv-roman@mail.ru>
2024-11-18 17:50:29 +0300

    -fix: На стр Продукта убран лишний отступ; -fix: Исправлены актуальные цвет и размер; -fix: Исправлена карточка продукта;

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "./ProductItem.module.css"
 import Image from "next/image"
 import Link from "next/link"
@@ -6,11 +7,12 @@ import ProductDimensions from "../ProductDimensions/ProductDimensions"
 import getGoods from "@/mock/goods"
 import { useBoundStore } from "@/store/StoreProvider"
 import { TcatalogGood } from "../../../types/goods"
-import { useEffect, useState } from "react"
+import { MouseEvent, useEffect, useState } from "react"
 import formatPriceNum from "@/utils/formatPriceNum"
 import { TcatalogGoodItem } from "../../../types/goodItem"
 import { STRAPI_URL } from "@/utils/api/endpoints"
 import clsx from "clsx"
+import { useRouter } from "next/navigation"
 
 type ProductItemProps = {
   good: TcatalogGoodItem
@@ -43,7 +45,7 @@ export default function ProductItem({ good, ofcanvasHandler }: ProductItemProps)
 
   return (
     <div className={clsx(styles.product, "animate_show")}>
-      <div className={styles.product_img_wrap}>
+      <Link href={`/product/${slug}-${id}`} className={styles.product_img_wrap}>
         <Image
           className={styles.product_img}
           src={`${STRAPI_URL}${images[0]}`}
@@ -52,42 +54,42 @@ export default function ProductItem({ good, ofcanvasHandler }: ProductItemProps)
           alt={name}
           priority={true}
         />
-        <button type="button" className={styles.add_cart_btn} onClick={addToCartHandler}>
-          {!inCart && <span className={styles.add_cart_title}>В корзину</span>}
-          {inCart && <span className={styles.add_cart_title}>Перейти в корзину</span>}
-          <span className={clsx(styles.add_cart_icon, inCart && styles.add_cart_icon__added)}>
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2.83284 5.79399L1.2539 13.294C1.12304 13.9155 1.59727 14.5 2.23245 14.5H13.7676C14.4027 14.5 14.877 13.9155 14.7461 13.294L13.1672 5.79399C13.0697 5.33125 12.6615 5 12.1886 5H3.81139C3.33851 5 2.93026 5.33125 2.83284 5.79399Z"
-                stroke="black"
-              />
-              <path
-                d="M5 5V4.5C5 2.84315 6.34315 1.5 8 1.5C9.65685 1.5 11 2.84315 11 4.5C11 5 11 5 11 5"
-                stroke="black"
-              />
-            </svg>
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2.83284 5.79399L1.2539 13.294C1.12304 13.9155 1.59727 14.5 2.23245 14.5H13.7676C14.4027 14.5 14.877 13.9155 14.7461 13.294L13.1672 5.79399C13.0697 5.33125 12.6615 5 12.1886 5H3.81139C3.33851 5 2.93026 5.33125 2.83284 5.79399Z"
-                fill="black"
-                stroke="black"
-              />
-              <path
-                d="M5 6V4.5C5 2.84315 6.34315 1.5 8 1.5C9.65685 1.5 11 2.84315 11 4.5C11 5 11 6 11 6"
-                stroke="black"
-              />
-            </svg>
-          </span>
-        </button>
-        <ul className={styles.product_parameters}>
-          <li className={styles.parameters_item}>
+        <div className={styles.product_parameters}>
+          <div className={styles.parameters_item}>
             <ProductColors colors={colors} actualColor={color} />
-          </li>
-          <li className={styles.parameters_item}>
+          </div>
+          <div className={styles.parameters_item}>
             <ProductDimensions sizes={sizes} actualSize={size} />
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+      </Link>
+      <button type="button" className={styles.add_cart_btn} onClick={addToCartHandler}>
+        {!inCart && <span className={styles.add_cart_title}>В корзину</span>}
+        {inCart && <span className={styles.add_cart_title}>Перейти в корзину</span>}
+        <span className={clsx(styles.add_cart_icon, inCart && styles.add_cart_icon__added)}>
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2.83284 5.79399L1.2539 13.294C1.12304 13.9155 1.59727 14.5 2.23245 14.5H13.7676C14.4027 14.5 14.877 13.9155 14.7461 13.294L13.1672 5.79399C13.0697 5.33125 12.6615 5 12.1886 5H3.81139C3.33851 5 2.93026 5.33125 2.83284 5.79399Z"
+              stroke="black"
+            />
+            <path
+              d="M5 5V4.5C5 2.84315 6.34315 1.5 8 1.5C9.65685 1.5 11 2.84315 11 4.5C11 5 11 5 11 5"
+              stroke="black"
+            />
+          </svg>
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2.83284 5.79399L1.2539 13.294C1.12304 13.9155 1.59727 14.5 2.23245 14.5H13.7676C14.4027 14.5 14.877 13.9155 14.7461 13.294L13.1672 5.79399C13.0697 5.33125 12.6615 5 12.1886 5H3.81139C3.33851 5 2.93026 5.33125 2.83284 5.79399Z"
+              fill="black"
+              stroke="black"
+            />
+            <path
+              d="M5 6V4.5C5 2.84315 6.34315 1.5 8 1.5C9.65685 1.5 11 2.84315 11 4.5C11 5 11 6 11 6"
+              stroke="black"
+            />
+          </svg>
+        </span>
+      </button>
       <Link href={`/product/${slug}-${id}`} className={styles.product_content}>
         <h3 className={styles.product_title}>{name}</h3>
         <span className={styles.product_price}>{formatPriceNum(price)} P</span>
